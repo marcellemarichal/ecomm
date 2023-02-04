bcc8c08d78dc9a8f4d660ed7f9e207d9efe94fe9
Marcelle Marichal da Rosa <marichaldarosa@gmail.com>
2023-02-04 15:04:47 -0300

    Expondo a documentação nas APIs

--- product/src/app.js
+++ product/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import db from "./config/dbConnect.js"
 import routes from "./routes/index.js"
+import swaggerUi from 'swagger-ui-express';
+import swaggerDoc from '../swagger/product.yaml' assert {type: "yaml"};
 
 db.on("error", console.log.bind(console, "Erro de conexão"))
 db.once("open", () => {
@@ -8,6 +10,8 @@ db.once("open", () => {
 })
 
 const app = express();
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(express.json())
 
