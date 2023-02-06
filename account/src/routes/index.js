620c7a1dd51458393bd604bae0021e31e028f139
Marcelle Marichal da Rosa <marichaldarosa@gmail.com>
2023-02-06 10:56:40 -0300

    removendo rotas desnecessárias criadas para teste anteriormente

--- account/src/routes/index.js
+++ account/src/routes/index.js
@@ -2,14 +2,7 @@ import express from "express";
 import usuarios from "./usersRoutes.js"
 
 const routes = (app) => {
-    app.route('/').get((req, res) => {
-        res.status(200).send({titulo: "Ecomm Users"})
-    })
-
-    app.use(
-        express.json(),
-        usuarios
-    )
+    app.use(express.json(), usuarios)
 }
 
 export default routes
--- product/src/routes/index.js
+++ product/src/routes/index.js
@@ -3,15 +3,7 @@ import categorias from "./categoriesRoutes.js"
 import produtos from "./productsRoutes.js"
 
 const routes = (app) => {
-    app.route('/').get((req, res) => {
-        res.status(200).send({titulo: "Ecomm Categories"})
-    })
-
-    app.use(
-        express.json(),
-        categorias,
-        produtos
-    )
+   app.use(express.json(), categorias, produtos)
 }
 
 export default routes
