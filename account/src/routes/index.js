c23b9cd1dfdaf13dedc0f4157101d10c4edb05b2
Marcelle Marichal <98181117+marcellemarichal@users.noreply.github.com>
2023-02-07 10:07:22 -0300

    Revert "Semana6 node express"

--- account/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import usuarios from "./usersRoutes.js"
-
-const routes = (app) => {
-    app.use(express.json(), usuarios)
-}
-
-export default routes
--- product/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import categorias from "./categoriesRoutes.js"
-import produtos from "./productsRoutes.js"
-
-const routes = (app) => {
-   app.use(express.json(), categorias, produtos)
-}
-
-export default routes
