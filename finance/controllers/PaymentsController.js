61c9b57ae62a94abe51ec65f96ddda473335d3f7
Marcelle Marichal da Rosa <marichaldarosa@gmail.com>
2023-03-05 14:01:35 -0300

    testes de pagamentos com erro 500

--- finance/controllers/PaymentsController.js
+++ finance/controllers/PaymentsController.js
@@ -11,7 +11,7 @@ class PagamentoController {
             })
             return res.status(200).json({ id, preco, nomeCartao, numeroCartao, vencimentoCartao, status, createdAt, updatedAt })
         } catch (error) {
-            return res.status(500).json(error.message)
+            return res.status(500).json(error)
         }
     }
 
