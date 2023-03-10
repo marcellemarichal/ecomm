0362afdb584fca92e0e9a9e451ce9d622f690716
Marcelle Marichal da Rosa <marichaldarosa@gmail.com>
2023-03-10 12:33:06 -0300

    autorizacao de produtos e categorias

--- product/src/routes/categoriesRoutes.js
+++ product/src/routes/categoriesRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import categoryController from "../controllers/categoriesController.js";
+import { authBearer } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router
     .get("/api/categories", categoryController.listarCategorias)
     .get("/api/categories/:id", categoryController.listarCategoriaPorID)
-    .post("/api/admin/categories", categoryController.cadastrarCategoria)
-    .put("/api/admin/categories/:id", categoryController.atualizarCategoria)
-    .delete("/api/admin/categories/:id", categoryController.excluirCategoria)
+    .post("/api/admin/categories", authBearer, categoryController.cadastrarCategoria)
+    .put("/api/admin/categories/:id", authBearer, categoryController.atualizarCategoria)
+    .delete("/api/admin/categories/:id", authBearer, categoryController.excluirCategoria)
 
 export default router;
--- product/src/routes/productsRoutes.js
+++ product/src/routes/productsRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import productController from "../controllers/productsController.js";
+import { authBearer } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router
     .get("/api/products", productController.listarProdutos)
     .get("/api/products/:id", productController.listarProdutoPorID)
-    .post("/api/admin/products", productController.cadastrarProduto)
-    .put("/api/admin/products/:id", productController.atualizarProduto)
-    .delete("/api/admin/products/:id", productController.excluirProduto)
+    .post("/api/admin/products", authBearer, productController.cadastrarProduto)
+    .put("/api/admin/products/:id", authBearer, productController.atualizarProduto)
+    .delete("/api/admin/products/:id", authBearer, productController.excluirProduto)
 
 export default router;
